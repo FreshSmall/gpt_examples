ca045189c2a66e8d5e6c277e6a3d5c248b0568fd
yinchao <ycsuper2819@gmail.com>
2024-10-31 13:35:55 +0800

    langchain修改

--- Chap5_01_LangChain/langchain_test.py
+++ Chap5_01_LangChain/langchain_test.py
@@ -1,8 +1,28 @@
 from dotenv import load_dotenv
-#from openai import OpenAI
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import SystemMessage, HumanMessage
+from langchain.llms import OpenAI
 from langchain_openai import OpenAI
+
+
 load_dotenv()
 
+
+def chat_openai():
+    llm = ChatOpenAI()
+    messages = [
+        SystemMessage(content="Translate the following from English into Italian"),
+        HumanMessage(content="hi!"),
+    ]
+    response = llm.invoke(messages)
+    print(response)
+#
+
+# def openai():
+#     llm = OpenAI()
+#     text = "why elon musk is so smart?"
+#     print(llm.invoke(text))
+
+
 if __name__ == '__main__':
-    llm = OpenAI()
-    llm.invoke("Hello how are you?")
+    chat_openai()
